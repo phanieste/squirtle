2f66d1d984238532af2150426f194a08d9854e52
John Anukem <johnanukem@dyn-160-39-134-92.dyn.columbia.edu>
2015-10-16 14:17:14 -0400

    Added some basic template stuff

--- app/app.py
+++ app/app.py
@@ -1,10 +1,28 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
+app.config.from_object('config')
 @app.route("/")
 def main():
-    return render_template('index.html')
+    user = {'nickname': 'John'}  # fake user
+    posts = [  # fake array of posts
+        { 
+            'author': {'nickname': 'John'}, 
+            'body': 'Beautiful day in Portland!' 
+        },
+        { 
+            'author': {'nickname': 'Susan'}, 
+            'body': 'The Avengers movie was so cool!' 
+        },
+        {
+        	'author': {'nickname' : 'Braxton'},
+        	'body' : 'Is that a world tour or your girls tour?'
+        }
+    ]
+    return render_template('index.html',
+                           title='Home',
+                           user=user,
+                           posts=posts)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
