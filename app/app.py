712a3cbd16f968cab56e5135cca1e2dba2db2ac9
John Anukem <johnanukem@dyn-160-39-135-201.dyn.columbia.edu>
2015-12-04 14:54:12 -0500

    Added ajax

--- app/app.py
+++ app/app.py
@@ -1,27 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import db
 import json
 import datetime as dt
-
+import requests
+import time
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return render_template('index.html')
+
+    return render_template('homepage.html')
 
 @app.route("/get_user/<username>")
 def get_user(username):
     return json.dumps(db.get_user(username))
 
-@app.route("/add_time/<ident>")
+@app.route("/add_time/<ident>", methods=['GET', 'POST'])
 def add_time(ident):
-    time = dt.datetime.now()
-    return json.dumps(db.new_person(ident, time))
+    if method.request == 'POST':
+        time = dt.datetime.now()
+        return time
 
-@app.route("/add_data/<ident>")
+@app.route("/add_data/<ident>", methods=['GET', 'POST'])
 def add_data(ident):
-    time = dt.datetime.now()
-    return str(db.update_times(ident, time))
+    if method.request == 'POST':
+        time = dt.datetime.now()
+        return str(db.update_times(ident, time))
+
+@app.route('/currentTime', methods=['GET', 'POST'])
+def currentTime():
+	if request.method == 'POST':
+         return self.client_address[0]
+	else:
+		return 'wrong'
 
 
 if __name__ == "__main__":
